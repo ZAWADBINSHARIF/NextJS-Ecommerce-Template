7bedbfaea3eecc8788bbd15bd683b72e09ed32da
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2026-02-14 00:44:17 +0600

    STORAGE_URL

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-// export const STORAGE_URL = process.env.NEXT_PUBLIC_STORAGE_URL;
-export const STORAGE_URL = '';
+export const STORAGE_URL = process.env.NEXT_PUBLIC_STORAGE_URL;
+// export const STORAGE_URL = '';
 export const STORE_NOT_FOUND = 'show_store_not_found';
 export const LOW_TO_HIGHT_PRICE = "price_asc";
 export const HIGHT_TO_LOW_PRICE = "price_desc";
